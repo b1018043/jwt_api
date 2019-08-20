9cc02e928295e119539d6f75d3ce61c56f0d9e3e
unknown <grey.fox.0417@gmail.com>
2019-08-20 14:12:46 +0900

    ADD: jsonからtokenを生成

--- auth/auth.go
+++ auth/auth.go
@@ -14,13 +14,45 @@ type tokenRes struct {
 	Token string `json:"token"`
 }
 
+type tokenRequest struct {
+	UserName string `json:"username"`
+	PassWord string `json:"password"`
+}
+
 // GetTokenHandker get token
 var GetTokenHandker = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	token := jwt.New(jwt.SigningMethodHS256)
 	claims := token.Claims.(jwt.MapClaims)
+	if r.Method == http.MethodGet {
+		claims["admin"] = true
+		claims["sub"] = "123456789"
+		claims["name"] = "fuga"
+		claims["iat"] = time.Now()
+		claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+		tokenString, err := token.SignedString([]byte(os.Getenv("SECRET_KEY")))
+		if err != nil {
+			w.Write([]byte("err"))
+		}
+		json.NewEncoder(w).Encode(&tokenRes{Token: tokenString})
+		return
+	}
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if r.Header.Get("Content-Type") != "application/json" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	var user tokenRequest
+	defer r.Body.Close()
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 	claims["admin"] = true
-	claims["sub"] = "123456789"
-	claims["name"] = "fuga"
+	claims["sub"] = user.PassWord
+	claims["name"] = user.UserName
 	claims["iat"] = time.Now()
 	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
 	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET_KEY")))
