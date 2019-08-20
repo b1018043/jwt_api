b47e51a6ba71d4becd50cebe29c324da538018fb
unknown <grey.fox.0417@gmail.com>
2019-08-20 11:08:49 +0900

    ADD JWT

---
+++ auth/auth.go
@@ -0,0 +1,34 @@
+package auth
+
+import (
+	"net/http"
+	"os"
+	"time"
+
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+// GetTokenHandker get token
+var GetTokenHandker = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["admin"] = true
+	claims["sub"] = "123456789"
+	claims["name"] = "fuga"
+	claims["iat"] = time.Now()
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET_KEY")))
+	if err != nil {
+		w.Write([]byte("err"))
+	}
+	w.Write([]byte(tokenString))
+})
+
+// JwtMiddleware check token
+var JwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
+	ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("SECRET_KEY")), nil
+	},
+	SigningMethod: jwt.SigningMethodHS256,
+})
--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/b1018043/jwt_api/auth"
+
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 type todo struct {
@@ -14,9 +17,19 @@ type todo struct {
 	User    string `json:"user"`
 }
 
+func envLoad() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
 func main() {
+	envLoad()
 	r := mux.NewRouter()
 	r.Handle("/todo", todos)
+	r.Handle("/private", auth.JwtMiddleware.Handler(privateTodo))
+	r.Handle("/auth", auth.GetTokenHandker)
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
@@ -27,6 +40,15 @@ var todos = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		Todo:    "ねる",
 		Process: "plan",
 		User:    "hoge",
+	}
+	json.NewEncoder(w).Encode(todo)
+})
+
+var privateTodo = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	todo := &todo{
+		Todo:    "aaa",
+		Process: "bbb",
+		User:    "ccc",
 	}
 	json.NewEncoder(w).Encode(todo)
 })
